b9679ad5ca880abd2a1ca484de7492914b94fbf2
lucacup00 <77009177+lucacup00@users.noreply.github.com>
2021-07-31 09:14:40 +0200

    Update mailsender.php

--- mailsender.php
+++ mailsender.php
@@ -6,14 +6,14 @@
 function send_mail($email, $oggetto, $messaggio, $path_allegato = null){
         $mail = new PHPMailer();
         $mail->IsSMTP();
-        $mail->Host = "mail.associazionegerardorestaini.it "; //indirizzo del server di posta in uscita
+        $mail->Host = " "; //indirizzo del server di posta in uscita
         $mail->SMTPDebug = 0;
         $mail->Port = 465; //porta del server di posta in uscita
  	    $mail->SMTPAuth = true;
         $mail->SMTPAutoTLS = false;
 	    $mail->SMTPSecure = 'ssl'; //tls o ssl informarsi presso il provider del vostro server di posta
-        $mail->Username = "prova@associazionegerardorestaini.it"; //la vostra mail
-        $mail->Password = "Luca2002"; //password per accedere alla vostra mail
+        $mail->Username = ""; //la vostra mail
+        $mail->Password = ""; //password per accedere alla vostra mail
         $mail->Priority= 1; //(1 = High, 3 = Normal, 5 = low)
         $mail->setFrom('prova@associazionegerardorestaini.it', 'Kite Force'); //impostazione del mittente
         $mail->AddAddress($email);
